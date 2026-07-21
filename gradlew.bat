f06e7a8243c6bf3403f740f71e6485e505b60795
yashrajput1281-hash <yashrajput1281@gmail.com>
2026-07-21 22:35:15 +0530

    Add files via upload

---
+++ gradlew.bat
@@ -0,0 +1,4 @@
+git add gradlew gradle/wrapper/gradle-wrapper.properties gradle/wrapper/gradle-wrapper.jar
+git commit -m "Add missing gradle wrapper files"
+git push origin <your-branch>
+cd my-android-app && chmod +x gradlew
